35bae6d5c62a17cea5270759221248cf051ccf79
Ana Fariya <anafariya@gmail.com>
2025-06-22 18:08:02 +0530

    removed admin Controllers

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,39 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { check } = require('express-validator');
-const {
-  login,
-  updateDoctorApproval,
-  getAllDoctors,
-} = require('../controllers/adminController');
 const { authAdmin } = require('../middleware/auth');
 const Provider = require('../models/Provider');
-
-router.post(
-  '/login',
-  [
-    check('email')
-      .isEmail()
-      .normalizeEmail()
-      .withMessage('Valid email is required'),
-    check('password').exists().withMessage('Password is required'),
-  ],
-  login
-);
-
-router.put(
-  '/doctors/approve',
-  authAdmin,
-  [
-    check('doctorId').not().isEmpty().withMessage('Doctor ID is required'),
-    check('isApproved')
-      .isBoolean()
-      .withMessage('Approval status must be boolean'),
-  ],
-  updateDoctorApproval
-);
-
-router.get('/doctors', authAdmin, getAllDoctors);
 
 // Provider Management Routes
 router.get('/providers', authAdmin, async (req, res) => {
